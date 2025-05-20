aaa698041487d2b3852f5973f61ba28aa44674ba
174382 <174382@cloud.rangitoto.school.nz>
2025-05-20 15:19:12 +1200

    added the menu and a loop to order as much as the user wants plus a list of things theyve ordered

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+budget_menu = {1 : "Kawakawa Spritzer",2 : "Pork and Puha Slider",3 : "Horopito Fish Collars",4 : "Kawakawa Muscles",5 : "Taro and Coconut Fritters",6 : "Kumara and Fennel Salad"}
+premium_menu = {1 : "Paua and Prawn Dumplings",2 : "Kina Canapes", 3 : "Kumara and Truffle Ravioli",4 : "Manuka Smoked Salmon", 5 : "Paua Porridge" }
+budget_price = 6
+premium_price = budget_price * 1.5
+
+
 name = input("What is the name for the order?")
+print(budget_menu)
+print(premium_menu)
+
+order_yn = input("Would you like to order?")
+while  order_yn.lower() == "yes":
+    order_stuff = input("What would you like to order?")
+    ordered = []
+    ordered.append(budget_menu[int(order_stuff)])
+    order_yn = input("Would you like to order?")
