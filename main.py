82022c473660cfdf5c3079957b87c0adaf02e0cd
174382 <174382@cloud.rangitoto.school.nz>
2025-05-22 10:00:11 +1200

    I made the customer have to at least order 3 items and appended the ordered items into a list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 budget_menu = {1 : "Kawakawa Spritzer",2 : "Pork and Puha Slider",3 : "Horopito Fish Collars",4 : "Kawakawa Muscles",5 : "Taro and Coconut Fritters",6 : "Kumara and Fennel Salad"}
-premium_menu = {1 : "Paua and Prawn Dumplings",2 : "Kina Canapes", 3 : "Kumara and Truffle Ravioli",4 : "Manuka Smoked Salmon", 5 : "Paua Porridge" }
+premium_menu = {7 : "Paua and Prawn Dumplings",8 : "Kina Canapes", 9 : "Kumara and Truffle Ravioli",10 : "Manuka Smoked Salmon", 11 : "Paua Porridge" }
 budget_price = 6
 premium_price = budget_price * 1.5
 
@@ -7,10 +7,17 @@ premium_price = budget_price * 1.5
 name = input("What is the name for the order?")
 print(budget_menu)
 print(premium_menu)
+ordered = []
 
+#I know theres magic numbers ill deal with them laters
 order_yn = input("Would you like to order?")
-while  order_yn.lower() == "yes":
-    order_stuff = input("What would you like to order?")
-    ordered = []
-    ordered.append(budget_menu[int(order_stuff)])
+while  order_yn.lower() == "yes" or len(ordered) < 3:
+    order_stuff = int(input("What would you like to order?"))
+    if 1 <= order_stuff <= 6:
+        ordered.append(budget_menu[int(order_stuff)])
+        print(ordered)
+    elif 7 <= order_stuff <= 11:
+        ordered.append(premium_menu[int(order_stuff)])
+        print(ordered)
     order_yn = input("Would you like to order?")
+
